cc4e738fe9979a4f0dafaf268ee86b7b86ef3b01
Mustafa Akdemir <mustafa.akdemir24@gmail.com>
2025-03-16 23:54:06 +0300

    Types of Functions, Keyword Arguments, Default Arguments, xargs, xxargs and Scope lessons have been finished.

--- Python_Mastery_Mosh/4_Functions/app.py
+++ Python_Mastery_Mosh/4_Functions/app.py
@@ -1,6 +1,48 @@
-def greet(first_name, last_name):
-    print(f"Hi {first_name} {last_name}")
-    print("Welcome aboard")
+def greet(name):
+    print(f"Hi {name}")
 
+# print(greet("Mosh"))
 
-greet("Mosh", "Hamedani")
+#### Keyword Arguments
+
+def increment(number, by):
+    return number + by
+
+# print(increment(2, by=1))
+
+##### Default Arguments
+
+def increment1(number, by=1):
+    return number + by
+
+# print(increment1(2, 5))
+
+###### xargs
+
+def multiply(*numbers):
+    total = 1
+    for number in numbers:
+        total *= number
+    return total
+
+# print(multiply(2, 3, 4, 5))
+
+##### xxargs
+
+def save_user(**user):
+    print(user["name"])
+
+# save_user(id=1, name="John", age=22)
+
+######### Scope
+
+message = "a"
+
+def greet1(name):
+    global message
+    message = "b"
+
+# greet1("Mosh")
+# print(message)
+
+##### Debugging
